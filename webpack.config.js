d1c97deb4007a1e8d440456926db82410d1145c8
Richmond <rief9805@student.su.se>
2017-01-07 17:14:37 +0100

    Added hot re-loading

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,14 @@ var BUILD_DIR = './static'
 var APP_DIR = './src'
 
 module.exports = {
-  entry: APP_DIR + '/index.jsx',
+//  entry: APP_DIR + '/index.jsx',
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/index.js'
+    ]
+  },
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js',
@@ -16,10 +23,21 @@ module.exports = {
   module : {
     loaders : [
       {
-        test : /\.jsx?/,
+        test : /\.js?/,
         exclude: /node_modules/,
         loader : 'babel'
       }
     ]
-  }
+  },
+  plugins : [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    contentBase: BUILD_DIR,
+    inline: true,
+    progress: true
+  },
 };
